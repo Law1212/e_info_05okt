ee01e2a707dcc4f6f0a2842205c9c63ee647d700
Law1212 <62355783+Law1212@users.noreply.github.com>
2020-11-06 17:22:51 +0100

    Not gonna lie, it was actually 6mins

--- kodol/Program.cs
+++ kodol/Program.cs
@@ -4,6 +4,10 @@ using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
 
+// Test nyiltszöveg: "ezapróbaszövegamitkódolunk"
+// Test titkosszöveg: "auto"
+// Test right result: ETTDRIUOSTHJEATAINDCDIEINE
+
 namespace kodol
 {
     class Program
@@ -11,8 +15,8 @@ namespace kodol
         static void Main(string[] args)
         {
             // 1.Feladat
-
-            Console.WriteLine("Adj meg egy nyilt szöveget: \n");
+            Console.Write("// 1.Feladat \n");
+            Console.WriteLine("Adj meg egy nyilt szöveget: ");
             string openText = Console.ReadLine();
 
             // 2.Feladat
@@ -21,86 +25,91 @@ namespace kodol
 
             // 3.Feladat
 
-            Console.WriteLine("\nNyílt szöveg: \n");
-            Console.WriteLine(openText + "\n");
+            Console.WriteLine("\n// 2. & 3. Feladat");
+            Console.WriteLine("Nyílt szöveg: " + openText + "\n");
 
             // 4. Feladat
 
-            Console.WriteLine("Adjon meg egy 1-5 karakter hosszú szöveget, ami csak az angol abc betüit tartalmazza! \n");
+            Console.Write("// 4.Feladat \n");
+            Console.WriteLine("Adjon meg egy 1-5 karakter hosszú szöveget, ami csak az angol abc betüit tartalmazza!");
             string secretText = Console.ReadLine();
             secretText.ToUpper();
 
             // 5. Feladat
 
+            Console.Write("\n// 5.Feladat \n");
             secretText = connectStrings(openText, secretText);
             Console.WriteLine("Secret text: " + secretText + "\n");
 
-
             // 6. Feladat
 
+            Console.Write("// 6.Feladat \n");
             Console.WriteLine("A titkosított szöveg: " + codeIt(openText ,secretText));
 
         }
-        // *2.Feladat
         static string inEnglish(string input)
         {
-            input = input.ToLower();
+            string returnString = "";
+            input = input.ToUpper();
+
+            // Converting the input into a char list
+
             List<char> charList = input.ToCharArray().ToList();
 
-            for(int i = 0; i < charList.Count; i++)
+            // Checking for hungarian uga buga characters
+
+            for (int i = 0; i < charList.Count; i++)
             {
-                if(charList[i] == 'á')
-                {
-                    charList[i] = 'a';
-                }
-                if (charList[i] == 'é')
-                {
-                    charList[i] = 'e';
-                }
-                if (charList[i] == 'í')
-                {
-                    charList[i] = 'i';
-                }
-                if (charList[i] == 'ö' || charList[i] == 'ő' || charList[i] == 'ó')
-                {
-                    charList[i] = 'o';
-                }
-                if (charList[i] == 'ü' || charList[i] == 'ű' || charList[i] == 'ú')
-                {
-                    charList[i] = 'u';
-                }
-
-            }
-            string returnString = "";
-            for (int j = 0; j < charList.Count; j++)
-                returnString += charList[j];
-
-            return returnString.ToUpper();
-        }
-                              
-        static string connectStrings(string OpenText, string Bemenet)
-        {
-
-            do {
-                Bemenet += Bemenet;
-            } while (Bemenet.Length < OpenText.Length);
+                if(charList[i] == 'Á')
+                    charList[i] = 'A';
                 
-            List<char> bemenetList = Bemenet.ToCharArray().ToList();
+                if (charList[i] == 'É')
+                    charList[i] = 'E';
+                
+                if (charList[i] == 'Í')
+                    charList[i] = 'I';
+                
+                if (charList[i] == 'Ö' || charList[i] == 'Ő' || charList[i] == 'Ó')
+                    charList[i] = 'O';
+                
+                if (charList[i] == 'Ü' || charList[i] == 'Ű' || charList[i] == 'Ú')
+                    charList[i] = 'U';
+                returnString += charList[i];
+            }
 
-            do {
-                bemenetList.RemoveAt(bemenetList.Count - 1);
-            } while (bemenetList.Count > OpenText.Length);
+            // Checking for speical characters
 
+            returnString = Regex.Replace(returnString, "[^A-Z]", "");
+
+            return returnString;
+        }
+        static string connectStrings(string OpenText, string SecretText)
+        {
             string returnString = "";
 
-            for (int i = 0; i < bemenetList.Count; i++)
-                returnString += bemenetList[i];
+            // Lengthing the SecretText
+
+            do {
+                SecretText += SecretText;
+            } while (SecretText.Length < OpenText.Length);
+
+            // Making sure that the two strings are the same length
+
+            List<char> SecretTextList = SecretText.ToCharArray().ToList();
+
+            while (SecretTextList.Count != OpenText.Length)
+                SecretTextList.RemoveAt(SecretTextList.Count - 1);
+            
+            // Adding together and returning the reformed string 
+
+            for (int i = 0; i < SecretTextList.Count; i++)
+                returnString += SecretTextList[i];
 
             return returnString.ToUpper();
         }
         static string codeIt(string OpenText ,string SecretText)
         {
-            // Reading .dat && creating a Jagged list to hold the data
+            // Reading .dat file && creating a Jagged list to hold the values
 
             StreamReader objInput = new StreamReader("Vtabla.dat", System.Text.Encoding.Default);
             string contents = objInput.ReadToEnd().Trim();
@@ -116,6 +125,9 @@ namespace kodol
                 }
                 Vtabla.Add(subVtabla);
             }
+
+            // Classifing the OpenText
+
             string returnAsClassified = "";
 
             for (int i = 0; i < OpenText.Length; i++)
@@ -136,6 +148,7 @@ namespace kodol
                         rowIndex = k;
                     }
                 }
+                returnAsClassified += Vtabla[columnIndex][rowIndex];
             }
             return returnAsClassified;
         }
